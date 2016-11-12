e802fd781a88d2abb000c04a192db7da7a801671
ImmortuonPC\Immortuon <jgkiax@yahoo.com>
2016-11-12 18:31:14 +0200

    base project
    created base project

---
+++ src/main/java/com/p4fun/otinane/OtinaneApplication.java
@@ -0,0 +1,12 @@
+package com.p4fun.otinane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtinaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtinaneApplication.class, args);
+	}
+}
