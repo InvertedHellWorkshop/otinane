ce93fbaf8dc07e0d51f3c16c4b51b3c0997cb633
Immortuon <jgkiax@yahoo.com>
2016-11-13 21:55:20 +0200

    Added User class

--- src/main/java/com/p4fun/otinane/model/User.java
+++ src/main/java/com/p4fun/otinane/model/User.java
@@ -1,7 +1,51 @@
 package com.p4fun.otinane.model;
 
 /**
- * Created by Immortuon on 13/11/2016.
+ * Created by Inverted Hell Workshop Death Crew on a cold and dreary day.
  */
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private String passwordConfirm;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Transient
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
 }
