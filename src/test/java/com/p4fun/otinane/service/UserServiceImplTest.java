c4c6db765a9b5a52ad7992e513fd07095241bf95
Immortuon <jgkiax@yahoo.com>
2016-12-02 22:12:42 +0200

    Refined UserServiceImpl class Refined WebSecurityConfig class Added UserServiceImplTest test class Added LoginTest test class

---
+++ src/test/java/com/p4fun/otinane/service/UserServiceImplTest.java
@@ -0,0 +1,50 @@
+package com.p4fun.otinane.service;
+
+/**
+ * Created by Inverted Hell Workshop Death Crew on a cold and dreary day.
+ */
+
+import com.p4fun.otinane.OtinaneApplication;
+import com.p4fun.otinane.model.User;
+import com.p4fun.otinane.repository.RoleRepository;
+import com.p4fun.otinane.repository.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.internal.verification.VerificationModeFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = OtinaneApplication.class)
+public class UserServiceImplTest {
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private RoleRepository roleRepository;
+
+
+    @Test
+    public void testAddUserSuccess() throws Exception{
+        String username="qwerty";
+        String password="12345678";
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPasswordConfirm(password);
+        user.setPassword(password);
+
+        userService.save(user);
+
+        Mockito.verify(userRepository, VerificationModeFactory.times(1)).save(Mockito.any(User.class));
+        Mockito.reset(userRepository);
+    }
+
+}
---
+++ src/test/java/com/p4fun/otinane/webapp/LoginTest.java
@@ -0,0 +1,82 @@
+package com.p4fun.otinane.webapp;
+
+/**
+ * Created by Inverted Hell Workshop Death Crew on a cold and dreary day.
+ */
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.fail;
+
+public class LoginTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "http://localhost:8080/login";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testLogin2() throws Exception {
+        driver.get(baseUrl + "/login");
+        driver.findElement(By.name("username")).clear();
+        driver.findElement(By.name("username")).sendKeys("TestAcc1");
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("12345678");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.findElement(By.linkText("Click here")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
