087c7061b4231e0e445a666040efff6484406d38
Immortuon <jgkiax@yahoo.com>
2016-12-08 21:41:08 +0200

    Added Spring Boot Actuators and configured them in application.properties

--- src/main/java/com/p4fun/otinane/web/UserController.java
+++ src/main/java/com/p4fun/otinane/web/UserController.java
@@ -18,7 +18,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
